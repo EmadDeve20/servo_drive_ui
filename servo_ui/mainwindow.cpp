23ae130fc3d7a76fd4e4e17222f1952dab0c07c9
EmadDeve20 <emaddeve20@gmail.com>
2022-12-08 11:20:23 +0330

    check the connection (first step :) )
    use the "try_connect_to_servo_device" function and do update the statusLabel label
    
    the connection happened by clicking on "Connect" Button

--- servo_ui/mainwindow.cpp
+++ servo_ui/mainwindow.cpp
@@ -21,14 +21,25 @@ MainWindow::~MainWindow()
 void MainWindow::on_connectChannelBtn_clicked()
 {
 
-    string channel = getEthercatChannel();
-
-    cout << "Channel: " << channel << endl;
-
+    if (try_connect_to_servo_device())
+        ui->statusLabel->setText("Connect");
+    else
+        ui->statusLabel->setText("Connection Failed!");
 }
 
 string MainWindow::getEthercatChannel()
 {
     string channel = ui->ethChannelEdt->text().toStdString();
     return channel;
+}
+
+bool MainWindow::try_connect_to_servo_device()
+{
+    string ifname = ui->ethChannelEdt->text().toStdString();
+    ec_master = delta::asda::ethercat::Master(ifname, slaves);
+
+    if (!ec_master.init())
+        return false;
+
+    return true;
 }
